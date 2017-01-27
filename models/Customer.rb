2df38c1236d500c67f95c1697cb6ebae600a4ec8
Colin McKean <colinmckeandigital@gmail.com>
2017-01-27 15:29:24 +0000

    improve save function to assign result to instance variable @id, this will handy

--- models/Customer.rb
+++ models/Customer.rb
@@ -1,7 +1,7 @@
 require_relative '../db/SqlRunner'
 
 class Customer
-  attr_reader :id, :customer_name, :funds
+  attr_accessor :id, :customer_name, :funds
   def initialize(options)
     @id = options['id'].to_i
     @customer_name = options['customer_name']
@@ -15,10 +15,22 @@ class Customer
             INTO customers (customer_name,funds)
             VALUES ('#{@customer_name}','#{@funds}') 
             RETURNING id;")
-    SqlRunner.run(sql).first['id'].to_i
+    @id = SqlRunner.run(sql).first['id'].to_i
   end
   def update()
+    sql = ("UPDATE customers
+            SET (customer_name, funds) =
+            ('#{@customer_name}', '#{@funds}')
+            WHERE id = #{@id};")
+            SqlRunner.run(sql)
+  end
 
+  def delete
+    sql = "DELETE 
+           FROM customers 
+           WHERE id=#{@id}"
+    SqlRunner.run(sql)
+    
   end
 
   def self.delete_all()
