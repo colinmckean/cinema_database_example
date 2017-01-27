0f01e645b35fd0ddadabb05d433c7a3556649d1e
Colin McKean <colinmckeandigital@gmail.com>
2017-01-27 14:26:40 +0000

    add data for film

--- console.rb
+++ console.rb
@@ -6,7 +6,9 @@ require_relative './models/Customers'
 customer1 = Customers.new('customer_name' => 'Alan',
   'funds' => '100')
 customer1.save
-
+film_1 = Films.new('title' => 'Star Wars',
+ 'price'=>'3.50')
+film_1.save
 binding.pry
 
 nil
