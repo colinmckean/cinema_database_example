9b11ab1bbe027855df86f7582e249a922ad3428c
Colin McKean <colinmckeandigital@gmail.com>
2017-01-27 16:12:28 +0000

    add save function to Ticket class

--- models/Ticket.rb
+++ models/Ticket.rb
@@ -1,5 +1,29 @@
-class Tickets
+class Ticket
+  attr_accessor :id, :customer_id, :film_id
   def initialize(options)
-    
+    @id = options['id'].to_i
+    @customer_id = options['customer_id'].to_i
+    @film_id = options['film_id'].to_i
   end
+
+  def save()
+    sql = ("INSERT
+            INTO tickets (customer_id, film_id)
+            VALUES ('#{@customer_id}', '#{@film_id}')
+            RETURNING id;")
+    @id = SqlRunner.run(sql).first['id'].to_i
+  end
+  def update()
+  end
+
+  def delete()
+  end
+
+  def self.delete_all()
+  end
+
+  def self.all()
+  end
+
+
 end
