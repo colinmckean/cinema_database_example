e1e62661f5c6312b3afa7cb75cad4ddd607052d5
Colin McKean <colinmckeandigital@gmail.com>
2017-01-27 14:25:55 +0000

    initialize and save funtion for Films

--- models/Films.rb
+++ models/Films.rb
@@ -1,5 +1,15 @@
 class Films
+  attr_reader :title, :price
   def initialize(options)
-    
+    @title = options['title']
+    @price = options['price']
+  end
+
+  def save()
+    sql = ("INSERT
+               INTO films (title,price)
+               VALUES ('#{@title}','#{@price}')
+               RETURNING *;")
+    SqlRunner.run(sql)
   end
 end
