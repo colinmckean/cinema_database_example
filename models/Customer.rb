c741e86488469d3660138981cc31f91cfed29114
Colin McKean <colinmckeandigital@gmail.com>
2017-01-27 15:39:19 +0000

    add update and delete functions

--- models/Customer.rb
+++ models/Customer.rb
@@ -1,7 +1,9 @@
 require_relative '../db/SqlRunner'
 
 class Customer
-  attr_accessor :id, :customer_name, :funds
+
+  attr_reader :id
+  attr_accessor :customer_name, :funds
   def initialize(options)
     @id = options['id'].to_i
     @customer_name = options['customer_name']
@@ -17,6 +19,7 @@ class Customer
             RETURNING id;")
     @id = SqlRunner.run(sql).first['id'].to_i
   end
+
   def update()
     sql = ("UPDATE customers
             SET (customer_name, funds) =
@@ -30,7 +33,6 @@ class Customer
            FROM customers 
            WHERE id=#{@id}"
     SqlRunner.run(sql)
-    
   end
 
   def self.delete_all()
