6b658ecca3f23c1891e66bf645ef44bf038e3e10
Colin McKean <colinmckeandigital@gmail.com>
2017-01-28 21:47:07 +0000

    add functions to show popular show times

--- models/Ticket.rb
+++ models/Ticket.rb
@@ -1,18 +1,18 @@
 class Ticket
-  attr_accessor :id, :customer_id, :film_id
+  attr_accessor :id, :customer_id, :film_id, :show_time
   def initialize(options)
     @id = options['id'].to_i
     @customer_id = options['customer_id'].to_i
     @film_id = options['film_id'].to_i
+    @show_time = options['show_time']
   end
 
   def save()
     sql = ("INSERT
-            INTO tickets (customer_id, film_id)
-            VALUES ('#{@customer_id}', '#{@film_id}')
+            INTO tickets (customer_id, film_id, show_time)
+            VALUES ('#{@customer_id}', '#{@film_id}', '#{show_time}')
             RETURNING id;")
     @id = SqlRunner.run(sql).first['id'].to_i
-
   end
   def update()
     sql = ("UPDATE tickets
@@ -27,6 +27,25 @@ class Ticket
             FROM tickets
             WHERE id = #{id};")
     SqlRunner.run(sql)
+  end
+
+  def self.popular_show_time
+    sql = ("SELECT show_time
+    FROM tickets
+    GROUP BY show_time
+    ORDER BY COUNT(*) DESC
+    LIMIT    1;")
+    SqlRunner.run(sql)[0]
+  end
+
+  def popular_time
+    sql = ("SELECT film_id, show_time
+    FROM tickets
+    WHERE film_id = #{@film_id}
+    GROUP BY show_time, film_id
+    ORDER BY COUNT(*) DESC
+    LIMIT    1;")
+    SqlRunner.run(sql)[0]["show_time"]
   end
 
   def self.delete_all()
