752d4e0dec8c6e2ccfeac2ccfa46911bd791b8fe
Colin McKean <colinmckeandigital@gmail.com>
2017-01-29 09:04:42 +0000

    add find_by_id function

--- models/Film.rb
+++ models/Film.rb
@@ -1,24 +1,25 @@
 class Film
   attr_reader :id
-  attr_accessor :title, :price
+  attr_accessor :title, :price, :tickets_avail
   def initialize(options)
     @id = options['id'].to_i
     @title = options['title']
     @price = options["price"].to_f
+    @tickets_avail = 5
   end
   #CRUD actions (create, read, update, delete) customers, films and tickets.
   def save()
     sql = ("INSERT
-     INTO films (title,price)
-     VALUES ('#{@title}','#{@price}')
+     INTO films (title,price, tickets_avail)
+     VALUES ('#{@title}','#{@price}', #{@tickets_avail})
      RETURNING id;")
     @id = SqlRunner.run(sql).first['id'].to_i
   end
 
   def update()
     sql = ("UPDATE films
-      SET (title, price) =
-      ('#{@title}', #{@price})
+      SET (title, price, tickets_avail) =
+      ('#{@title}', #{@price}, #{tickets_avail})
       WHERE id = #{@id}")
     SqlRunner.run(sql)
   end
@@ -46,6 +47,12 @@ class Film
            ON f.id = t.film_id
            WHERE f.id = #{@id};"
     SqlRunner.run(sql).map { |ticket_holder| TicketHolder.new(ticket_holder) }
+  end
+  
+  def self.find_by_id(id_to_find)
+    sql = ("SELECT * FROM films
+           WHERE id = #{id_to_find};")
+    SqlRunner.run(sql).first
   end
 
   def self.delete_all()
